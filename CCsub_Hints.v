19b7be00c361e8e8ba59853d83985bf8b14bd1f7
Avish Kathpal <kathpal.adil@gmail.com>
2024-10-01 12:37:21 -0400

    more hints from ed's repo

--- CCsub_Hints.v
+++ CCsub_Hints.v
@@ -8,16 +8,16 @@ Ltac destruct_union_mem H :=
 
 Hint Extern 1 (wf_typ ?Γ ?T) =>
 match goal with
-| H : wf_typ ?Γ (typ_capt _ ?P) |- _ =>
+| H : wf_typ ?Γ (typ_cse _ ?P) |- _ =>
   inversion H; subst; (match goal with
                        | H : wf_typ ?Γ (typ_arr ?T _) |- _ =>
                          inversion H; subst; assumption
                        end)
 end : core.
 
-Hint Extern 1 (wf_cset ?Γ ?C) =>
+Hint Extern 1 (wf_cse ?Γ ?C) =>
 match goal with
-| H : typing ?Γ _ (typ_capt ?C _) |- _ =>
+| H : typing ?Γ _ (typ_cse ?C _) |- _ =>
   let P := fresh "P" in
   pose proof (proj2 (proj2 (typing_regular _ _ _ H))) as P; inversion P; assumption
 end : core.
@@ -27,6 +27,35 @@ Hint Extern 1 (wf_env ?Γ) =>
 match goal with
 | H : sub ?Γ _ _ |- _ => apply (proj1 (sub_regular _ _ _ H))
 end : core.
+
+
+Hint Extern 1 (wf_env ?E) =>
+  match goal with
+  | H: subcset _ _ _ |- _ => apply (proj1 (subcapt_regular _ _ _ H))
+  | H: sub _ _ _ |- _ => apply (proj1 (sub_regular _ _ _ H))
+  | H: typing _ _ _ |- _ => apply (proj1 (typing_regular _ _ _ H))
+  end : core.
+
+Hint Extern 1 (wf_typ ?E ?T) =>
+  match goal with
+  | H: typing E _ T |- _ => apply (proj2 (proj2 (typing_regular _ _ _ H)))
+  | H: type E T _ |- _ => apply (proj1 (proj2 (sub_regular _ _ _ H)))
+  end : core.
+
+
+Hint Extern 1 (wf_cse ?E ?T) =>
+  match goal with
+  | H: subcset E T _ |- _ => apply (proj1 (proj2 (subcapt_regular _ _ _ H)))
+  | H: subcset E _ T |- _ => apply (proj2 (proj2 (subcapt_regular _ _ _ H)))
+  end : core.
+
+Hint Extern 1 (cset ?T) =>
+  let go E := apply (cset_from_wf_cset E); auto in
+  match goal with
+  | H: typing ?E _ (T # _) |- _ => go E
+  | H: subcset ?E T _ |- _ => go E
+  | H: subcset ?E _ T |- _ => go E
+  end : core.
 
 Ltac rewrite_set_facts_in H :=
   match type of H with
