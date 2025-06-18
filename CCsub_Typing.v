a065d94cab4c2a7440a200dcad242111d590ac90
fluistro <sam674526@gmail.com>
2025-06-18 19:39:44 -0400

    Update ccsub_typing, some proofs left admitted

--- CCsub_Typing.v
+++ CCsub_Typing.v
@@ -13,7 +13,8 @@ Lemma typing_weakening : forall Γ Θ Δ e T S,
   typing (Δ ++ Γ) S e T ->
   wf_ctx (Δ ++ Θ ++ Γ) S ->
   typing (Δ ++ Θ ++ Γ) S e T.
-Proof with simpl_env;
+Admitted.
+(* Proof with simpl_env;
            eauto using wf_typ_weakening,
                        wf_typ_from_wf_ctx_typ,
                        sub_weakening,
@@ -21,8 +22,8 @@ Proof with simpl_env;
   intros * Typ. remember (Δ ++ Γ).
   generalize dependent Δ.
   induction Typ; intros Δ EQ Ok; subst...
-  - Case "typing_abs".
-    pick fresh X and apply typing_abs...
+    (* Case "typing_abs". *)
+  - pick fresh X and apply typing_abs...
     lapply (H0 X); [intros K | auto].
     simpl_env in *.
     rewrite <- concat_assoc.
@@ -48,7 +49,7 @@ Proof with simpl_env;
   - Case "typing_unbox".
     apply typing_unbox...
     apply wf_cse_weakening... 
-Qed.
+Qed. *)
 
 Lemma typing_weakening_store : forall Γ e T S1 S2 S3,
   typing Γ (S1 ++ S3) e T ->
@@ -72,10 +73,11 @@ Lemma typing_narrowing : forall Q Δ Γ X P e T S,
   sub Γ S P Q ->
   typing (Δ ++ [(X, bind_sub Q)] ++ Γ) S e T ->
   typing (Δ ++ [(X, bind_sub P)] ++ Γ) S e T.
-Proof with eauto using wf_ctx_narrowing, wf_typ_ignores_sub_bindings, sub_narrowing, subcapt_narrowing.
+Admitted.
+(* Proof with eauto using wf_ctx_narrowing, wf_typ_ignores_sub_bindings, sub_narrowing, subcapt_narrowing.
   intros * PsubQ Typ.
   assert (PureP : pure_type P).
-  { enough (PureQ : pure_type Q) by (applys sub_pure_type PsubQ; eauto* ).
+  { enough (PureQ : pure_type Q) by (applys sub_pure_type PsubQ; eauto ).
     forwards (_ & WfCtx & _): typing_regular Typ.
     apply wf_ctx_tail in WfCtx.
     inversion WfCtx...
@@ -117,13 +119,14 @@ Proof with eauto using wf_ctx_narrowing, wf_typ_ignores_sub_bindings, sub_narrow
     eapply wf_cse_narrowing...
   - Case "typing_sub".
     apply typing_sub with (R := R)...
-Qed.
+Qed. *)
 
 Lemma typing_narrowing_typ : forall D Q Γ Δ X C P e T S,
   typing (Δ ++ [(X, bind_typ (D # Q))] ++ Γ) S e T ->
   sub Γ S (C # P) (D # Q) ->
   typing (Δ ++ [(X, bind_typ (C # P))] ++ Γ) S e T.
-Proof with eauto*.
+Admitted.
+(* Proof with eauto.
   intros * Typ Sub.
   assert (CsubD_PsubQ_WfC_WfD_PureP_PureQ : subcapt Γ S C D /\ sub Γ S P Q /\ wf_cse Γ S C /\ wf_cse Γ S D /\ pure_type P /\ pure_type Q).
   { dependent induction Sub... }
@@ -183,7 +186,7 @@ Proof with eauto*.
   - Case "typing_sub".
     apply typing_sub with (R := R)...
     eapply sub_narrowing_typ...
-Qed.
+Qed. *)
 
 (* ********************************************************************** *)
 (** ** Inversion of typing (13) *)
@@ -196,11 +199,12 @@ Lemma typing_inv_abs : forall Γ S1 e1 T S,
     typing ([(x, bind_typ S1)] ++ Γ) S (open_ve e1 x (cse_fvar x)) (open_ct S2 (cse_fvar x)) /\
     wf_typ ([(x, bind_typ S1)] ++ Γ) S (open_ct U2 (cse_fvar x)) /\
     sub ([(x, bind_typ U1)] ++ Γ) S (open_ct S2 (cse_fvar x)) (open_ct U2 (cse_fvar x)).
-Proof with auto.
+Admitted.
+    (* Proof with auto.
   intros * Typ.
   dependent induction Typ; intros U1 U2 D Sub.
-  - Case "typing_abs".
-    inversion Sub; subst.
+  (* Case "typing_abs". *)
+  - inversion Sub; subst.
     inversion select (sub _ _ _ _); subst.
     split...
     exists T1.
@@ -214,7 +218,7 @@ Proof with auto.
     applys sub_regular Sub'.
   - Case "typing_sub".
     eauto using (sub_transitivity T).
-Qed.
+Qed. *)
 
 Lemma typing_inv_tabs : forall Γ S1 e1 T S,
   typing Γ S (Λ [S1] e1) T ->
@@ -223,22 +227,23 @@ Lemma typing_inv_tabs : forall Γ S1 e1 T S,
   /\ exists S2, exists L, forall X, X ∉ L ->
     typing ([(X, bind_sub U1)] ++ Γ) S (open_te e1 X) (open_tt S2 X) /\
     sub ([(X, bind_sub U1)] ++ Γ) S (open_tt S2 X) (open_tt U2 X).
-Proof with simpl_env; auto.
+Admitted.
+    (* Proof with simpl_env; auto.
   intros * Typ.
   dependent induction Typ; intros U1 U2 D Sub.
-  - Case "typing_tabs".
-    inversion Sub; subst.
+  (* Case "typing_tabs". *)
+  - inversion Sub; subst.
     inversion select (sub _ _ _ _); subst.
     split...
     exists T1.
-    exists (L `union` L0).
+    exists (L `union`a L0).
     intros Y ?.
     repeat split...
     rewrite_nil_concat.
     eapply typing_narrowing with (Q := S1)...
   - Case "typing_sub".
     eauto using (sub_transitivity T).
-Qed.
+Qed. *)
 
 Lemma typing_inv_let : forall Γ e k T S,
   typing Γ S (let= e in k) T ->
@@ -246,13 +251,13 @@ Lemma typing_inv_let : forall Γ e k T S,
     typing Γ S e (C # R)
     /\ exists L, forall x, x ∉ L ->
       typing ([(x, bind_typ (C # R))] ++ Γ) S (open_ve k x (cse_fvar x)) T.
-Proof with eauto*.
+Proof with eauto.
   intros * Typ.
   dependent induction Typ...
   destruct (IHTyp e k ltac:(reflexivity)) as [C [R0 [eTyp [L kTyp]]]].
   exists C, R0.
   split...
-  exists (L `u`A dom Γ).
+  exists (L `union`a EnvImpl.dom Γ).
   intros y NotIn.
   specialize (kTyp y ltac:(clear - NotIn; fsetdec)).
   apply typing_sub with (R := R)...
